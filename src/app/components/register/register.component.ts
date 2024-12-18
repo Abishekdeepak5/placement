fbf6519d9f1776ad4e86779c45d6ae165a6af065
Abishek Aranganathan <abisheka@avecsage.com>
2024-12-18 12:18:44 +0530

    register/login component create

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent implements OnInit{
+
+  ngOnInit(): void {}
+}
